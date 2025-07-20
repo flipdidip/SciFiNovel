21fd4195ac5dd44c6ce0f4d38143c4c90dccd3ee
Philip <philip.materna@googlemail.com>
2025-07-20 10:50:12 +0200

    Removed sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
 import "./Layout.css";
 
 const Layout = () => {
   return (
     <div className="layout">
       <Header />
-      <div className="layout-body">
-        <Sidebar />
-        <main className="main-content">
-          <Outlet />
-        </main>
-      </div>
+      <main className="main-content">
+        <Outlet />
+      </main>
     </div>
   );
 };
